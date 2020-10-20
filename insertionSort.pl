0bfecb62bd1a1ce8a2eac706ed91fab079336ceb
kevin <kevin.mouse@hotmail.com>
2020-10-20 12:10:25 -0500

    Codigo: Cambios minimos en 'insertionSort.pl'

--- insertionSort.pl
+++ insertionSort.pl
@@ -13,7 +13,11 @@
         Lista2 = insertionSort(Lista[1 a len(Lista)-1])
         regresar insertar(Lista[0],Lista2).
 */
-
+fill([],_,_,0).
+fill([H|T],RMin,RMax,N) :-
+    random(RMin,RMax,H),
+    fill(T,RMin,RMax,X),
+    N is X+1.
 
 insertar(X, [], [X]):- !.
 insertar(X, [X1|L1], [X, X1|L1]):- X=<X1, !.
@@ -21,4 +25,11 @@ insertar(X, [X1|L1], [X1|L]):- insertar(X, L1, L).
 
 insertionSort([], []):- !.
 insertionSort([X|L], S):- insertionSort(L, S1), insertar(X, S1, S).
+
+ordenamientoAlAzar(NumElementos,RMin,RMax) :-
+    RMin =< RMax,
+    fill(Lista,RMin,RMax,NumElementos),
+    write("La lista desordenada es"),writeln(Lista),
+    insertionSort(Lista,Res),
+    write("La lista ordenada es "),writeln(Res).
 
